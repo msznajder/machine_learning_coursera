a454c3716880b44af8b87a5bc45c014f884ee563
Michal Sznajder <msznajder@gmail.com>
2017-11-20 17:52:26 +0100

    Exercise 6 - Support Vector Machines - finished tuning C and sigma params.

--- ex6_support_vector_machines/dataset3Params.m
+++ ex6_support_vector_machines/dataset3Params.m
@@ -22,13 +22,33 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+% vals = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+% [p, q] = meshgrid(vals, vals);
+% vals_pairs = [p(:) q(:)];
+% 
+% error = 100000
+% for row=1:size(vals_pairs, 1)
+%     candidate_C = vals_pairs(row, 1);
+%     candidate_sigma = vals_pairs(row, 2);
+%     model= svmTrain(Xval, yval, candidate_C, @(x1, x2) gaussianKernel(x1, x2, candidate_sigma));
+%     pred = svmPredict(model, Xval);
+%     
+%     candidate_error = mean(double(pred ~= yval));
+%     
+%     if candidate_error < error
+%         C = candidate_C;
+%         sigma = candidate_sigma;
+%         error = candidate_error;
+%     end;
+    
+% end
 
-
-
-
-
-
+C = 1
+sigma = 0.1
 
 % =========================================================================
 
+
+    
+    
 end
